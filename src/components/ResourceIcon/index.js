dadd8b44782a54ca4b68c9a81b4e9b6e170c1aaf
jferragut <31458614+jferragut@users.noreply.github.com>
2020-08-08 01:36:03 -0400

    format adjust

--- src/components/ResourceIcon/index.js
+++ src/components/ResourceIcon/index.js
@@ -5,8 +5,23 @@ export default function ResourceIcon(props){
     const {icon,title,bg,color} = props
     return (
       <div className="my-4">
-        <div className="d-flex align-items-center justify-content-center mx-auto" style={{borderRadius: "10px",background: bg,width:"100px",height:"100px"}}>
-          <FontAwesomeIcon color={color} icon={icon} style={{width:"75px",height:"65px"}}/>
+        <div 
+            className="d-flex align-items-center justify-content-center mx-auto" 
+            style={{
+                borderRadius: "10px",
+                background: bg, 
+                width:"100px",
+                height:"100px"
+            }}
+        >
+          <FontAwesomeIcon 
+            color={color} 
+            icon={icon} 
+            style={{
+                width:"75px",
+                height:"65px"
+            }}
+          />
         </div>
         <h3 className="my-3 text-center">{title}</h3>
       </div>
