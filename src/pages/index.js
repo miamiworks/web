ebbb409354afc0aba9f06e5b92062adb153eeef2
jferragut <31458614+jferragut@users.noreply.github.com>
2020-08-09 19:36:58 -0400

    added mobile detect,resize hook and separated view components for skills

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,30 @@ import {
   faCalendarStar,
   faChartNetwork 
 } from "@fortawesome/pro-regular-svg-icons"
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect"
 import Layout from "../components/Layout"
 import Section from "../components/Section"
+import KeySkillsDesktop from "../components/KeySkillsDesktop"
+import KeySkillsMobile from "../components/KeySkillsMobile"
 import { TopNav } from "../components/TopNav"
 import ResourceIcon from "../components/ResourceIcon"
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from "react-bootstrap/Container"
-import Nav from "react-bootstrap/Nav"
-import Tab from "react-bootstrap/Tab"
-import CourseCard from "../components/CourseCard"
 import cardTopImg from "../images/cardTopClass.png"
 import gaLogo from "../images/gen-assembly-logo.png"
 import geeksLogo from "../images/4geeks-logo.png"
+import {useWindowSize} from "../utils/hooks"
 
 export default function Home() {
     const [skill,setSkill] = useState(null);
     const [course,setCourse] = useState(null);
+    const [width,height] = useWindowSize();
 
     const heroFeatures = [
       {
@@ -98,6 +105,7 @@ export default function Home() {
         title: "Fullstack Web Developer",
         timeframe: "12 weeks",
         buttonText: "Learn More",
+        id: "ga-fullstack",
       },
       {
         topImgAlt: "4geeks",
@@ -106,6 +114,7 @@ export default function Home() {
         title: "Fullstack Web Developer",
         timeframe: "12 weeks",
         buttonText: "Learn More",
+        id: "geeks-fullstack",
       },
       {
         topImgAlt: "Gen Assembly",
@@ -114,6 +123,7 @@ export default function Home() {
         title: "Web Immersive",
         timeframe: "12 weeks",
         buttonText: "Learn More",
+        id: "ga-fullstack",
       },
       {
         topImgAlt: "Gen Assembly",
@@ -122,6 +132,7 @@ export default function Home() {
         title: "Data Science Immersion",
         timeframe: "12 weeks",
         buttonText: "Learn More",
+        id: "ga-fullstack",
       },
     ]
 
@@ -161,115 +172,23 @@ export default function Home() {
             <p className="display-5 text-warning h2 mb-5 pb-5">
               to land jobs of the future
             </p>
-            <Tab.Container
-              id="skills-main"
-              defaultActiveKey="software-engineering"
-            >
-              <Row>
-                <Col xs={12} sm={4} lg={3} className="align-items-stretch">
-                  <Nav className="flex-column skills-nav h-100 align-content-between justify-content-between p-2">
-                    {keySkillsMenu.map(item => (
-                      <Nav.Link
-                        eventKey={item.key}
-                        key={item.key}
-                        className="text-white"
-                        onClick={() => setSkill(null)}
-                      >
-                        {item.label}
-                      </Nav.Link>
-                    ))}
-                  </Nav>
-                </Col>
-                <Col className="right-container">
-                  <Tab.Content className="h-100">
-                    <Tab.Pane eventKey="software-engineering" className="h-100">
-                      {skill === "software-engineering" ? (
-                        <Row className="h-100">
-                          <Col className="p-0">
-                            <Row className="mb-4 h-100 mx-lg-n2">
-                              {courseData.map((item, index) => (
-                                <Col key={index} md={3} className="px-lg-2">
-                                  <CourseCard
-                                    topImgAlt={item.topImgAlt}
-                                    topImg={item.topImg}
-                                    logoSrc={item.logoSrc}
-                                    title={item.title}
-                                    timeframe={item.timeframe}
-                                    buttonText={item.buttonText}
-                                    setter={setCourse}
-                                    value={course}
-                                  />
-                                </Col>
-                              ))}
-                            </Row>
-                          </Col>
-                        </Row>
-                      ) : (
-                        <Row className="h-100">
-                          <Col
-                            className="software-eng-img p-0 d-flex flex-column justify-content-end align-items-end"
-                            xs={5}
-                            md={4}
-                          >
-                            <div className="skills-overlay p-3 w-100 h-50">
-                              <h3 className="">Software Engineering</h3>
-                              <p className="mb-2">Salary range in Miami</p>
-                              <p className="skills-overlay-salary">
-                                $55-110K per year
-                              </p>
-                            </div>
-                          </Col>
-                          <Col className="right-column p-3 h-100">
-                            <Row className="mb-4">
-                              <Col>
-                                {" "}
-                                <h4 className="title">What they do?</h4>
-                                <p>
-                                  process of analyzing user requirements and
-                                  then designing, building, and testing software
-                                  application which will satisfy those
-                                  requirements
-                                </p>
-                              </Col>
-                            </Row>
-                            <Row className="mb-4">
-                              <Col>
-                                <h4 className="title">
-                                  What’s the job outlook?
-                                </h4>
-                                <p>
-                                  process of analyzing user requirements and
-                                  then designing, building, and testing software
-                                  application which will satisfy those
-                                  requirements
-                                </p>
-                              </Col>
-                            </Row>
-                            <Row className="position-absolute fixed-bottom p-3">
-                              <Col>
-                                <p className="mb-4">
-                                  <strong>
-                                    4 local course providers available
-                                  </strong>
-                                </p>
-                                <button
-                                  className="btn btn-outline-warning"
-                                  onClick={() =>
-                                    setSkill("software-engineering")
-                                  }
-                                >
-                                  See Courses
-                                </button>
-                              </Col>
-                            </Row>
-                          </Col>
-                        </Row>
-                      )}
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Col>
-              </Row>
-            </Tab.Container>
+            {isMobile || width < 990 ? (
+              <KeySkillsMobile
+                keySkillsMenu={keySkillsMenu}
+                courseData={courseData}
+                setCourse={setCourse}
+                setSkill={setSkill}
+                skill={skill}
+              />
+            ) : (
+              <KeySkillsDesktop
+                keySkillsMenu={keySkillsMenu}
+                courseData={courseData}
+                setCourse={setCourse}
+                setSkill={setSkill}
+                skill={skill}
+              />
+            )}
           </Container>
         </Section>
 
