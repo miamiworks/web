f193fccef4b78c1a6b3bba85237cd17d912cd6db
jferragut <31458614+jferragut@users.noreply.github.com>
2020-08-09 12:07:03 -0400

    added basic layout for key-skills and navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ import ResourceIcon from "../components/ResourceIcon"
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Tab from "react-bootstrap/Tab"
 import { faSuitcase,faCommentMedical,faCalendarStar,faChartNetwork } from "@fortawesome/pro-regular-svg-icons"
 
 export default function Home() {
@@ -45,15 +47,41 @@ export default function Home() {
       { httpEquiv: "X-UA-Compatible", content: "IE=edge,chrome=1" },
     ]
 
+    const keySkillsMenu = [
+      {
+        label: "Software Engineering",
+        key: "software-engineering",
+      },
+      {
+        label: "UX Design",
+        key: "ux-design",
+      },
+      {
+        label: "Data Science",
+        key: "data-science",
+      },
+      {
+        label: "Cyber Security",
+        key: "cyber-security",
+      },
+      {
+        label: "Digital Marketing",
+        key: "digital-marketing",
+      },
+      {
+        label: "Machine Learning",
+        key: "machine-learning",
+      },
+      {
+        label: "IT Administration",
+        key: "it-administration",
+      },
+    ]
+
     return (
-      <Layout
-        bodyClass="home"
-        description=""
-        lang="en"
-        meta={meta}
-      >
+      <Layout bodyClass="home" description="" lang="en" meta={meta}>
         <Section className="home-hero h-100 position-relative">
-          <TopNav/>
+          <TopNav />
           <Container>
             <Row className="mb-5 mx-auto">
               <Col xs={12}>
@@ -76,30 +104,63 @@ export default function Home() {
           </Container>
         </Section>
 
-        <Section className="explore-jobs">
+        <Section className="explore-jobs"></Section>
 
+        <Section className="key-skills d-flex align-items-center justify-content-center">
+          <Container>
+            <h2 className="text-white mb-3">
+              <span className="text-warning">Learn</span> Key Skills
+            </h2>
+            <p className="display-5 text-warning h2 mb-5 pb-5">
+              to land jobs of the future
+            </p>
+            <Tab.Container
+              id="skills-main"
+              defaultActiveKey="software-engineering"
+              className="feature-block"
+            >
+              <Row>
+                <Col xs={3}>
+                  <Nav className="flex-column skills-nav">
+                    {keySkillsMenu.map(item => (
+                      <Nav.Link
+                        eventKey={item.key}
+                        key={item.key}
+                        className="text-white"
+                      >
+                        {item.label}
+                      </Nav.Link>
+                    ))}
+                  </Nav>
+                </Col>
+                <Col>
+                  <Tab.Content>
+                    <Tab.Pane eventKey="software-engineering">
+                      <Row>
+                        <Col className="software-eng-img">
+                          <div className="skills-overlay">
+                            <h3>Software Engineering</h3>
+                            <p>Salary range in Miami</p>
+                            <p className="skills-overlay-salary"></p>
+                          </div>
+                        </Col>
+                        <Col></Col>
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Col>
+              </Row>
+            </Tab.Container>
+          </Container>
         </Section>
-        
-        <Section className="key-skills">
-          
-        </Section>
-        
-        <Section className="career-coaching">
 
-        </Section>
+        <Section className="career-coaching"></Section>
 
-        <Section className="events">
+        <Section className="events"></Section>
 
-        </Section>
+        <Section className="job-search"></Section>
 
-        <Section className="job-search">
-
-        </Section>
-
-        <Section className="sponsors">
-
-        </Section>
-
+        <Section className="sponsors"></Section>
       </Layout>
     )
 }
