e708460bfcdfe247c334d4cee9d0d9f5713ca653
jferragut <31458614+jferragut@users.noreply.github.com>
2020-08-10 19:20:02 -0400

    firebase changes to make querying work

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from "react"
+import React,{useEffect} from "react"
 import { TopNav } from "../TopNav"
 import SEO from "../Seo"
 import "../../style.scss"
+import { FirebaseAuth, FirebaseDB } from "../Firebase/FirebaseApp"
 
+
+FirebaseAuth
+  .signInAnonymously()
+  .catch(function (error) {
+    var errorCode = error.code
+    var errorMessage = error.message
+    console.error(`Error: ${errorCode}. ${errorMessage}`)
+  })
+
+  
 const Layout = ({
     children,
     bodyClass,
@@ -17,6 +28,18 @@ const Layout = ({
     coverImage,
     coverDescription,
 }) => {
+    useEffect(()=>{
+  
+      FirebaseDB.collection("events")
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            console.log(`${doc.id} => ${doc.data()}`)
+          })
+        })
+      
+      
+    },[]);
     return (
       <div className={bodyClass}>
         <SEO
