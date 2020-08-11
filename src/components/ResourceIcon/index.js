12e6d61ab1ee1f0662fe064767586e2a6c113c42
jferragut <31458614+jferragut@users.noreply.github.com>
2020-08-11 17:33:35 -0400

    porting to svg from fontawesome pro

--- src/components/ResourceIcon/index.js
+++ src/components/ResourceIcon/index.js
@@ -2,26 +2,39 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function ResourceIcon(props){
-    const {icon,title,bg,color} = props
+    const {icon,title,bg} = props
+
     return (
       <div className="my-4">
-        <div 
-            className="d-flex align-items-center justify-content-center mx-auto" 
-            style={{
-                borderRadius: "10px",
-                background: bg, 
-                width:"100px",
-                height:"100px"
-            }}
+        <div
+          className="d-flex align-items-center justify-content-center mx-auto"
+          style={{
+            borderRadius: "10px",
+            background: bg,
+            width: "100px",
+            height: "100px",
+          }}
         >
-          <FontAwesomeIcon 
-            color={color} 
-            icon={icon} 
+          {props.icon}
+          {/* <svg
+            fill={color}
+            src={icon}
+            }style={{
+              width: "75px",
+              height: "65px",
+              "& path": {
+                fill: color
+              }
+            }
+          ></svg> */}
+          {/* <FontAwesomeIcon
+            color={color}
+            icon={icon}
             style={{
-                width:"75px",
-                height:"65px"
+              width: "75px",
+              height: "65px",
             }}
-          />
+          /> */}
         </div>
         <h3 className="my-3 text-center">{title}</h3>
       </div>
