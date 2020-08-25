cd8518565290ceaa3cd424507dc4f814d304aefe
jferragut <31458614+jferragut@users.noreply.github.com>
2020-08-25 16:45:47 -0400

    fixed navbar brand size on mobile #66

--- src/components/TopNav/index.js
+++ src/components/TopNav/index.js
@@ -27,9 +27,16 @@ export const TopNav = ({ links }) => {
 
     const data = useStaticQuery(graphql`
         query {
-            file(relativePath: { regex: "/Logo_BlueCircle.png/" }) {
+            desktop: file(relativePath: { regex: "/Logo_BlueCircle.png/" }) {
                 childImageSharp {
                     fixed(width: 96, height: 96) {
+                    ...GatsbyImageSharpFixed
+                    }
+                }
+            }
+            mobile: file(relativePath: { regex: "/Logo_BlueCircle.png/" }) {
+                childImageSharp {
+                    fixed(width: 75, height: 75) {
                     ...GatsbyImageSharpFixed
                     }
                 }
@@ -37,45 +44,85 @@ export const TopNav = ({ links }) => {
         }
     `)
 
+    const sources = [
+      data.mobile.childImageSharp.fixed,
+      {
+        ...data.desktop.childImageSharp.fixed,
+        media: `(min-width: 768px)`,
+      },
+    ]
+
     return (
-        <header className={"main-nav fixed-top" + (navBackground ? ' sticky-active' : '')}>
-            <div className="header-topbar text-center py-3">Give your job search a home with CareerScore</div>
-            <Navbar bg="transparent" sticky="top" variant="dark" expand="lg" className="px-5">
-                <Navbar.Brand href="#home">
-                    <Img fixed={data.file.childImageSharp.fixed} />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
-                        {Array.isArray(links) && links.map((item, i)=> {
-                            const Component = item.component;
-                            return (Component !== undefined) ?
-                                <Component key={i} className="ml-5 nav-link" />
-                            :(item.to.charAt(0) === "#") ?
-                                <Link key={i} activeClass="active" className="ml-5 nav-link" to={item.to.substring(1)} spy={true} smooth={true} duration={500} >
-                                    {item.label}
-                                </Link>
-                                : item.target !== "_blank" ?
-                                <Nav.Link className="ml-5"
-                                onClick={() => navigate(item.url)}
-                                key={i}>
-                                    {item.label}
-                                </Nav.Link>
-                                :
-                                <a key={i}
-                                    className="ml-5 nav-link"
-                                    href={item.to}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    key={i}
-                                    >
-                                    {item.label}
-                                </a>
-                        })}
-                        <Button onClick={() => window.open('https://submit.miamitech.works/jobs','noopener') } variant="outline-warning">Post a job</Button>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </header>
+      <header
+        className={
+          "main-nav fixed-top" + (navBackground ? " sticky-active" : "")
+        }
+      >
+        <div className="header-topbar text-center py-3">
+          Give your job search a home with CareerScore
+        </div>
+        <Navbar
+          bg="transparent"
+          sticky="top"
+          variant="dark"
+          expand="lg"
+          className="px-5"
+        >
+          <Navbar.Brand href="#home">
+            <Img fixed={sources} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              {Array.isArray(links) &&
+                links.map((item, i) => {
+                  const Component = item.component
+                  return Component !== undefined ? (
+                    <Component key={i} className="ml-5 nav-link" />
+                  ) : item.to.charAt(0) === "#" ? (
+                    <Link
+                      key={i}
+                      activeClass="active"
+                      className="ml-5 nav-link"
+                      to={item.to.substring(1)}
+                      spy={true}
+                      smooth={true}
+                      duration={500}
+                    >
+                      {item.label}
+                    </Link>
+                  ) : item.target !== "_blank" ? (
+                    <Nav.Link
+                      className="ml-5"
+                      onClick={() => navigate(item.url)}
+                      key={i}
+                    >
+                      {item.label}
+                    </Nav.Link>
+                  ) : (
+                    <a
+                      key={i}
+                      className="ml-5 nav-link"
+                      href={item.to}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      key={i}
+                    >
+                      {item.label}
+                    </a>
+                  )
+                })}
+              <Button
+                onClick={() =>
+                  window.open("https://submit.miamitech.works/jobs", "noopener")
+                }
+                variant="outline-warning"
+              >
+                Post a job
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
     )
 }
