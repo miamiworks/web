92a08d146965185389136ddd0b228a28e95a224d
jferragut <31458614+jferragut@users.noreply.github.com>
2020-08-26 12:02:29 -0400

    fix footer and reinstall missing dep #73

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,20 +2,30 @@ import React from "react"
 import { useStaticQuery,graphql } from "gatsby"
 import Img from "gatsby-image"
 import { navigate } from '@reach/router';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav,Col,Row } from 'react-bootstrap';
 import "./style.scss";
 
 export const Footer = () => {
 
     return (
-        <footer className="footer bg-primary text-white py-4">
-            <div className="container row mx-auto">
-                <div className="col-4 col-md-3 text-left">
-                    <a href="https://www.beaconcouncil.com/terms-conditions/" target="_blank" rel="noopener noreferrer">Legal</a>
-                </div>
-                <div className="col-4 col-md-6 text-center made-miami">Made in Miami 🌴</div>
-                <div className="col-4 col-md-3 text-right">{new Date().getFullYear()} MiamiTech.Works</div>
-            </div>
-        </footer>
+      <footer className="footer bg-primary text-white py-4">
+        <Row className="container mx-auto">
+          <Col xs={12} md={3} className="text-center text-md-left">
+            <a
+              href="https://www.beaconcouncil.com/terms-conditions/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Legal
+            </a>
+          </Col>
+          <Col xs={12} md={6} className="text-center made-miami">
+            Made in Miami 🌴
+          </Col>
+          <Col xs={12} md={3} className="text-center text-md-right">
+            {new Date().getFullYear()} MiamiTech.Works
+          </Col>
+        </Row>
+      </footer>
     )
 }
